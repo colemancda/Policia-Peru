7d314da3b79c384d5ec188803a2a8fa40c1113b6
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-06-22 20:41:10 -0500

    Created Xcode project

---
+++ CorePolicia/CorePolicia.h
@@ -0,0 +1,16 @@
+//
+//  CorePolicia.h
+//  CorePolicia
+//
+//  Created by Alsey Coleman Miller on 6/22/16.
+//  Copyright © 2016 ColemanCDA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CorePolicia.
+FOUNDATION_EXPORT double CorePoliciaVersionNumber;
+
+//! Project version string for CorePolicia.
+FOUNDATION_EXPORT const unsigned char CorePoliciaVersionString[];
+
---
+++ CorePolicia/Store.swift
@@ -0,0 +1,17 @@
+//
+//  Store.swift
+//  Policia Peru
+//
+//  Created by Alsey Coleman Miller on 6/22/16.
+//  Copyright © 2016 ColemanCDA. All rights reserved.
+//
+
+import SwiftFoundation
+
+
+public class Store {
+    
+    public static let shared = Store()
+    
+    
+}
